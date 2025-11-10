0946d7bd908c9daacf5b3942b4227be9d8faa8f6
iv3ta10 <fridrichovaiveta@gmail.com>
2025-11-10 20:00:35 +0100

    Review changes

--- main.py
+++ main.py
@@ -1,61 +1,71 @@
-ukoly = []
+ukoly = {}
 
 def pridat_ukol():
-    uziv_nazev = input("Zadejte název úkolu: ")
-    uziv_popis = input("Zadejte popis úkolu: ")
-    cely_ukol = " - ".join([uziv_nazev, uziv_popis])
-    if uziv_nazev == "" or uziv_popis == "":
-        print("Zadal jste prázdný vstup")
-        pridat_ukol()
-    else:
-        ukoly.append(cely_ukol)
+    while True:
+        uziv_nazev = input("Zadejte název úkolu: ")
+        uziv_popis = input("Zadejte popis úkolu: ")
+        if uziv_nazev == "" or uziv_popis == "":
+            print("Zadal jste prázdný vstup.")
+            continue
+
+        if uziv_nazev in ukoly:
+            print("Tento úkol již existuje.")
+            continue
+
+        ukoly[uziv_nazev] = uziv_popis
         print(f"Úkol '{uziv_nazev}' byl přidán.")
+        break
 
 def zobrazit_ukoly():
-    if ukoly == []:
+    if ukoly == {}:
         print("Seznam úkolů je prázdný.")
-    else:
-        print("Seznam úkolů:")
-        for index, ukol in enumerate(ukoly):
-            print(f"{index + 1}. {ukol}")
+        return
+    
+    print("Seznam úkolů:")
+    for index, ukol in enumerate(ukoly):
+        print(f"{index + 1}. {ukol} - {ukoly[ukol]}")
 
 def odstranit_ukol():
     zobrazit_ukoly()
-    odstraneny_ukol = int(input("Zadejte číslo úkolu, který chcete odstranit: "))
-    try:
-        if ukoly[odstraneny_ukol - 1] in ukoly:
-            print(f"Úkol '{ukoly[odstraneny_ukol - 1]}' byl odstraněn.")
-            ukoly.remove(ukoly[odstraneny_ukol - 1])
-    except IndexError:
-        print("Tento úkol neexistuje.")
+    while True:
+        try:
+            odstraneny_index = int(input("Zadejte číslo úkolu, který chcete odstranit: "))
+        except(KeyboardInterrupt):
+            exit()
+        except:
+            print("Zadal jste neplatný vstup.")
+            continue
+
+        if odstraneny_index <= 0 or odstraneny_index > len(ukoly):
+            print("Tento úkol neexistuje.")
+            continue
+
+        odstraneny_klic = list(ukoly)[odstraneny_index - 1]
+        print(f"Úkol '{odstraneny_klic}' byl odstraněn.")
+        del ukoly[odstraneny_klic]
+        break
 
 def hlavni_menu():
-    print(
+    while True:
+        print(
 """
 1. Přidat nový úkol
 2. Zobrazit všechny úkoly
 3. Odstranit úkol
 4. Konec programu
 """)
-    try:
-        uziv_vstup = int(input("Vyberte možnost(1-4): "))
-        if uziv_vstup == 1:
+        uziv_vstup = input("Vyberte možnost(1-4): ")
+        if uziv_vstup == "1":
             pridat_ukol()
-            hlavni_menu()
-        elif uziv_vstup == 2:
+        elif uziv_vstup == "2":
             zobrazit_ukoly()
-            hlavni_menu()
-        elif uziv_vstup == 3:
+        elif uziv_vstup == "3":
             odstranit_ukol()
-            hlavni_menu()
-        elif uziv_vstup == 4:
+        elif uziv_vstup == "4":
             print("Konec programu.")
-        elif uziv_vstup > 4 or uziv_vstup < 1:
+            break
+        else: 
             print("Neplatná volba.")
-            hlavni_menu()
-    except:
-        print("Neplatná volba.")
-        hlavni_menu()
 
 if __name__ == "__main__":
     hlavni_menu()
